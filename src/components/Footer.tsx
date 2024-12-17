21f1f6cddde11cf7cb2ca9d37772e47c032076bf
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2024-12-17 18:57:25 +0100

    feat: add footer and clean comments

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer : React.FC = () => {
+
+  return (
+    <div className="text-center">
+      <div className="flex justify-center items-center gap-4 mb-8 text-xs text-gray-600">
+       <a href="https://alejandrobolano.vercel.app" target="_blank">Copyright © 2024. All rights reserved.</a>
+      </div>
+    </div>
+  );
+};
