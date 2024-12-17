12e2b44bb367fd588e093ac95e9ffa8f368253be
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2024-12-17 19:12:10 +0100

    feat: add code link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ export const Footer : React.FC = () => {
   return (
     <div className="text-center">
       <div className="flex justify-center items-center gap-4 mb-8 text-xs text-gray-600">
-       <a href="https://alejandrobolano.vercel.app" target="_blank">Copyright © 2024. All rights reserved.</a>
+       <a href="https://alejandrobolano.vercel.app" target="_blank">Copyright © 2024 😍 All rights reserved.</a>
+        <a href="https://github.com/alejandrobolano/Merry-Xmas" target="_blank">💻 Code</a>
       </div>
     </div>
   );
